12930c95206e1a2dde37e58e94787a5a7569e489
Sandra Penadés <spenlop1@upv.edu.es>
2026-02-04 13:17:13 +0100

    NavMesh 2D configurado para el movimiento del colono

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -1,53 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class MovementController : MonoBehaviour
 {
-    // Velocidad de movimiento
-    [SerializeField] private float speed = 3f;
-
-    // Direccion
-    private Vector3 targetPosition;
-    private bool isMoving = false;
+    // Configuramos el NavMeshAgent
+    private NavMeshAgent agent;
 
     void Start()
     {
-        targetPosition = transform.position;
+        // Obtenemos la referencia del agente
+        agent = GetComponent<NavMeshAgent>();
+
+        // Para que no gire
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+
     }
 
     void Update()
     {
-        // Detectar clic izquierdo
-        if (Input.GetMouseButtonDown(0))
+        // Detectar clic derecho
+        if (Input.GetMouseButtonDown(1))
         {
-            SetTargetPosition();
-        }
-
-        // Moverse si no esta en el destino
-        if (isMoving)
-        {
-            MoveCharacter();
+            SetDestinationToMouse();
         }
     }
 
-    void SetTargetPosition()
+    void SetDestinationToMouse()
     {
-        // Se convierte el punto del raton a coordenadas del mundo (x, y, z) en metros
-        targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        targetPosition.z = 0;
-
-        isMoving = true;
-    }
-
-    void MoveCharacter()
-    {
-        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
-
-        // Si la distancia al objetivo es practicamente nula, para
-        if (Vector3.Distance(transform.position, targetPosition) < 0.01f)
-        {
-            isMoving = false;
-        }
+        // Convertimos el raton a mundo
+        Vector3 targetPos = UnityEngine.Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        targetPos.z = 0;
+        
+        // El agente calcula la ruta
+        agent.SetDestination(targetPos);
     }
 }
